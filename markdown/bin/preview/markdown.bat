46e868edc6135bbe122da903557cdf2892bfc757
hudamin <hudamin@baidu.com>
2014-07-31 11:35:07 +0800

    update markdown/preview

--- markdown/bin/preview/markdown.bat
+++ markdown/bin/preview/markdown.bat
@@ -1,11 +1,12 @@
 setlocal
 
 set FILENAME=%1
-set MARKDOWNCMD=C:\markdown\markdown.exe
-set ROOT=C:\markdown
+set MARKDOWNCMD=C:\markdown_preview\markdown.exe
+set ROOT=C:\markdown_preview
 
 %MARKDOWNCMD% "%FILENAME%" > %ROOT%\tmp\__tmp__.md.html 
 type "%ROOT%\tpl\header.tpl.html" "%ROOT%\tmp\__tmp__.md.html" "%ROOT%\tpl\footer.tpl.html" > "%ROOT%\tmp\preview.html" 
-start "%ROOT%\tmp\preview.html"
+rem must no double-quotes
+start %ROOT%\tmp\preview.html
 
 endlocal

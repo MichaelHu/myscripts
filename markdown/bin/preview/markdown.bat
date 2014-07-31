03d8d9b238d6c551fb5f36f9c261b85e81fbb7a4
hudamin <hudamin@baidu.com>
2014-07-31 16:25:28 +0800

    master: update markdown preview

--- markdown/bin/preview/markdown.bat
+++ markdown/bin/preview/markdown.bat
@@ -1,12 +1,15 @@
 setlocal
 
-set FILENAME=%1
+set TMPFILE=%1.__tmp__
 set MARKDOWNCMD=C:\markdown_preview\markdown.exe
 set ROOT=C:\markdown_preview
 
-%MARKDOWNCMD% "%FILENAME%" > %ROOT%\tmp\__tmp__.md.html 
+type %1 %ROOT%\file_empty_line > %TMPFILE%
+
+%MARKDOWNCMD% "%TMPFILE%" > "%ROOT%\tmp\__tmp__.md.html" 
 type "%ROOT%\tpl\header.tpl.html" "%ROOT%\tmp\__tmp__.md.html" "%ROOT%\tpl\footer.tpl.html" > "%ROOT%\tmp\preview.html" 
 rem must no double-quotes
 start %ROOT%\tmp\preview.html
 
+del %TMPFILE%
 endlocal
--- markdown/bin/preview/markdown.sh
+++ markdown/bin/preview/markdown.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-FILENAME=$1
+TMPFILE=$1.__tmp__
 MARKDOWNCMD=/Users/hudamin/projects/git/git-myscripts/markdown/bin/preview/markdown.out
 ROOT=/Users/hudamin/projects/git/git-myscripts/markdown/bin/preview
 
-$MARKDOWNCMD "$1" | cat "$ROOT/tpl/header.tpl.html" - "$ROOT/tpl/footer.tpl.html" \
+cat "$1" "$ROOT/file_empty_line" > "$TMPFILE"
+
+$MARKDOWNCMD "$TMPFILE" | cat "$ROOT/tpl/header.tpl.html" - "$ROOT/tpl/footer.tpl.html" \
     > "$ROOT/tmp/preview.html" \
     && open "$ROOT/tmp/preview.html"
 
-
+rm "$TMPFILE"

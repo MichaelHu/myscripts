89f1bc09550c7444e34db0a2131dc4119053f03d
hudamin <hudamin@baidu.com>
2014-08-06 22:52:45 +0800

    master: markdown/preview: add local option

--- markdown/bin/preview/markdown.sh
+++ markdown/bin/preview/markdown.sh
@@ -3,10 +3,15 @@
 TMPFILE=$1.__tmp__
 MARKDOWNCMD=/Users/hudamin/projects/git/git-myscripts/markdown/bin/preview/markdown.out
 ROOT=/Users/hudamin/projects/git/git-myscripts/markdown/bin/preview
+TPL=tpl
+
+if [ "$2" == "local" ]; then
+    TPL=tpl_local
+fi
 
 cat "$1" "$ROOT/file_empty_line" > "$TMPFILE"
 
-$MARKDOWNCMD "$TMPFILE" | cat "$ROOT/tpl/header.tpl.html" - "$ROOT/tpl/footer.tpl.html" \
+$MARKDOWNCMD "$TMPFILE" | cat "$ROOT/$TPL/header.tpl.html" - "$ROOT/$TPL/footer.tpl.html" \
     > "$ROOT/tmp/preview.html" \
     && open "$ROOT/tmp/preview.html"
 

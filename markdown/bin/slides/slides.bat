3e0c8f4d8718945c0bed8d202a0f7290ab43abf2
hudamin <hudamin@baidu.com>
2014-09-17 18:46:59 +0800

    master: markdown/bin/slides update to support windows and unix

---
+++ markdown/bin/slides/slides.bat
@@ -0,0 +1,26 @@
+setlocal enabledelayedexpansion
+
+set TMPFILE=%1.__tmp__
+set SRCDIR=
+set PREVIEWFILE=%1.preview.html
+set MARKDOWNCMD=C:\slides\win\markdown.exe
+set ROOT=C:\slides
+set REVEAL=
+
+for /f %%i in ("%TMPFILE%") do set SRCDIR=%%~pi
+if "%2" == "reveal" set REVEAL="_reveal"
+
+rem echo !SRCDIR!
+rem echo !REVEAL!
+
+type %1 %ROOT%\file_empty_line > %TMPFILE%
+
+%MARKDOWNCMD% "%TMPFILE%" > "%ROOT%\tmp\__tmp__.md.html" 
+type "%ROOT%\tpl\header!REVEAL!.tpl.html" "%ROOT%\tmp\__tmp__.md.html" "%ROOT%\tpl\footer!REVEAL!.tpl.html" > "%PREVIEWFILE%" 
+mkdir !SRCDIR!\lib
+xcopy /e /y /q %ROOT%\lib !SRCDIR!\lib
+rem must no double-quotes
+start %PREVIEWFILE%
+
+del %TMPFILE%
+endlocal

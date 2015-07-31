55323d2823753295d1809c90d7a84d79acb25435
hudamin <hdm258i@gmail.com>
2015-07-31 20:01:27 +0800

    master: add didi/pms.bat

---
+++ didi/pms.bat
@@ -0,0 +1,42 @@
+rem @description Generate PMS file list
+rem @example
+rem call pms.bat BASEREVISION 
+
+echo off
+
+setlocal enabledelayedexpansion
+
+set BASEREVISION=%1
+set CHANGELISTFILE=_change.lst
+set REPOPATH=""
+set CURREVISION=""
+set tmpfile=_tmp.tmp
+
+svn up 
+svn diff -r!BASEREVISION! | findstr "^Index:" | sort > !CHANGELISTFILE! 
+
+svn info | findstr "^URL:" > %tmpfile%
+for /f "tokens=2" %%i in (%tmpfile%) do set REPOPATH=%%i 
+
+svn info | findstr "^Revision:" > %tmpfile%
+for /f "tokens=2" %%i in (%tmpfile%) do set CURREVISION=%%i 
+
+rem Trim last space
+set REPOPATH=%REPOPATH:~0,-1%
+
+rem type %CHANGELISTFILE%
+
+rem Grep items and remove duplicated items
+for /f "tokens=2" %%i in (!CHANGELISTFILE!) do (
+    if not "!LAST!"=="%%i" (
+        if not "!LAST!"=="" (
+            echo %REPOPATH%/!LAST! %CURREVISION%
+        )
+    )
+    set LAST=%%i
+)
+
+del %CHANGELISTFILE%
+del %tmpfile%
+
+endlocal
